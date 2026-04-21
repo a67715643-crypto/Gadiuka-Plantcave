7aa41a4f105d4c5705c1f44945d5d770b5e21c2c
a67715643-crypto <a67715643@gmail.com>
2026-04-21 19:25:27 +0300

    Add files via upload

---
+++ Gadiuka Plantcave.py
@@ -0,0 +1,292 @@
+import colorama
+
+colorama.init()
+
+print(colorama.Back.LIGHTWHITE_EX)
+print(colorama.Fore.BLACK)
+
+print("""instruction:
+      print takeX2 to take;
+      print jumpX2 to jump;
+      print goX2 to go;
+      print openX2 to open;
+      print fightX2 to fight.""")
+
+print(colorama.Back.LIGHTBLACK_EX)
+
+act = input()
+
+print('mineshaft')
+print('go?')
+
+input()
+
+if input() == 'go':
+ print(colorama.Back.YELLOW)
+ print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+ print(colorama.Back.GREEN)
+ print('Me: What is this? A big cave with plants... This is strange for Underworld')
+ print("""
+ (-----)
+(  . .  )
+ (__U__)
+    |
+____|____
+|       |
+ \_____/""")
+print('Sunflowey: Hi!')
+print('Me: Hi? Who are you?')
+print('Sunflowey: I am Sunflowey!')
+print('Me: What are you doing here?')
+print('Sunflowey: U... Uh... Never mind')
+print('Sunflowey: This is... END')
+
+print("""
+      _________________
+      |               |)
+      |     *   *     | )
+      |    \_____/    | )
+      |_______________|)
+xxxxxx99             99xxxxxx
+       99           99
+        99         99
+         99>     <99
+         99>     <99
+         99>     <99
+         99>     <99
+   
+ x====                 ====x
+   x====             ====x
+     x====         ====x
+       x====     ====x
+         x==== ====x
+          \/\/ \/\/
+""")
+print('Sunflowey: WAHAHAHAHAHA! WAHAHAHAHA!')
+print("""Sunflowey: fight
+           fight""")
+
+print('run')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""Sunflowey: fight
+           fight""")
+
+print('run')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""Sunflowey: fight
+           fight""")
+
+print('run')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""Sunflowey: fight
+           fight""")
+print('You were hurt a lot!')
+
+print(colorama.Back.LIGHTYELLOW_EX)
+print('Alert! Alert! Gadiuka is here!')
+
+print('call for help')
+input('you called for help...')
+
+print("""Gadiuka: heal
+         heal""")
+print('You are good now!')
+
+print(colorama.Back.GREEN)
+print('run')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""Sunflowey: fight
+           fight""")
+
+print('run')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""Sunflowey: fight
+           fight""")
+
+print('run')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""Sunflowey: fight
+           fight""")
+
+print('run')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""Sunflowey: fight
+           fight""")
+
+print('run')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""Sunflowey: fight
+           fight""")
+
+print('run')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print('Sunflowey is holding you. You can not run.')
+
+    print("""Sunflowey: fight
+               fight""")
+
+    print('You failed.')
+
+    print("""Sunflowey: revive
+           revive""")
+    print('You were hurt a lot!')
+
+    print('Sunflowey: Now no one will save you! Call for help! Just try!')
+    print('call for help')
+    print('you called for help...')
+    print('but no one came...')
+    print('Sunflowey: You did  not listen to me. No one would came!')
+    print('Sunflowey: WAHAHAHAHAHA! WAHAHAHAHA!')
+
+    print("""Sunflowey: fight
+               fight
+               fight
+               fight
+               fight
+               fight
+               fight
+               fight""")
+
+    print('Sunflowey: How... How did you...')
+
+    print('Gadiuka is here!')
+    print("""Gadiuka: fight
+         fight
+         fight
+         fight
+         fight
+         fight
+         fight
+         fight
+         fight
+         fight""")
+
+    print('Sunflowey: NOOOOOOO! STOP IT! PLEASE!')
+
+    print('You win!')
+
+    print("""
+     (---_-)
+    (  . .  ] 
+     {__n__)
+        |
+    ____|__-_
+    | x     |
+     \___x_/""")
+
+    print('Choose: mercy | fight')
+
+    input()
+
+    if input() == 'mercy':
+        print("""Sunflowey: Why? Why are you saving me?""")
+        print('Sunflowey: If you won\'t defeat me I will become a monster one more time!')
+
+        print('Choose: mercy | fight')
+
+        input()
+
+        if input() == 'mercy':
+            print('Why are you so good to me? I can\'t understand!')
+
+        print('Sunflowey')
+        print('take?')
+
+        input()
+
+        if input() == 'take':
+            print('Gadiuka: Veeery very good! Let\'s go back to the Gadiuka Cafe!')
+
+        print('mineshaft')
+        print('go?')
+
+        input()
+
+        if input() == 'go':
+            print(colorama.Back.YELLOW)
+            print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+print("""
+       _________
+      /        /
+     /________/
+""")
+print('trapdoor')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""        coffee = 40
+        tea = 20
+        cookies = 5
+        pie = 80""")
+    print('Gadiuka: It is over now! Everything in the Underworld is fought now! We won! But where is the python?')
+    print('Sunflowey: HE ATE ALL THE PIES!')
+    print('Me: Hehehe!')
+    print('Gadiuka: Hehehe!')
+    print('Sunflowey: Hehehe!')
+    print('🐍')
+    print('Sunflowey: Thank you and Gadiuka for saving me! Now we will live together!')
+    print('Gadiuka: Yes!')
+    print('Me: Yes!')
+    print('Sunflowey: And... It was the finale! We won the fight with the Underworld!')
+    print('Gadiuka: Ye.')
+    print('Me: Ye.')
+    print("""
+     (-----)
+    (  . .  )
+     (__U__)
+        |
+    ____|____
+    |       |
+     \_____/""")
+    print('🐍')
+
+    print(colorama.Back.LIGHTWHITE_EX)
+    print('It was the finale!🐍')
+    print("""Thanks for playing!🐍
+    Made on Python
+    Powered by PyCharm
+    Made by Gadiuka
+
+    🐍Special thanks for Python and PyCharm""")
